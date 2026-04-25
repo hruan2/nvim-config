1c3e94b22bc0b80271f179b141f42e05ac9958ba
Henron Ruan <t7x9q7@u.northwestern.edu>
2026-04-25 11:06:06 -0500

    actually fixed spell check now

--- lua/test/remap.lua
+++ lua/test/remap.lua
@@ -96,10 +96,22 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
     end,
 })
 
+-- Filetypes to enable spellcheck
+local spell_types = { "text", "plaintex", "typst", "gitcommit", "markdown", "tex" }
+
+-- Set global spell option to false initially to disable it for all file types
+vim.opt.spell = false
+
+-- Create an augroup for spellcheck to group related autocommands
+vim.api.nvim_create_augroup("Spellcheck", { clear = true })
+
+-- Create an autocommand to enable spellcheck for specified file types
 vim.api.nvim_create_autocmd({ "FileType" }, {
-    pattern = { "plaintex", "text", "markdown", "tex" },
-    callback = function ()
-        vim.opt_local.spell = true
-        vim.opt_local.spelllang = "en_us"
-    end,
+  group = "Spellcheck", -- Grouping the command for easier management
+  pattern = spell_types, -- Only apply to these file types
+  callback = function()
+    vim.opt_local.spell = true -- Enable spellcheck for these file types
+    vim.opt_local.spelllang = "en_us"
+  end,
+  desc = "Enable spellcheck for defined filetypes", -- Description for clarity
 })
