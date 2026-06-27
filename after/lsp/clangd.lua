348061691f3a689a58b1df5bafe6530cedc343b1
Henron Ruan <t7x9q7@u.northwestern.edu>
2026-06-27 15:52:33 -0500

    added custom lsp config for clangd

---
+++ after/lsp/clangd.lua
@@ -0,0 +1,26 @@
+---@type vim.lsp.Config
+return {
+    cmd = { 'clangd', '--clang-tidy', '--background-index', '--function-arg-placeholders=0' },
+    filetypes = { 'c', 'cpp', 'objc', 'objcpp' },
+    root_markers = {
+        '.clangd',
+        '.clang-tidy',
+        '.clang-format',
+        'compile_commands.json',
+        'compile_flags.txt',
+        'configure.ac', -- AutoTools
+        '.git',
+    },
+    get_language_id = function(_, ftype)
+        local t = { objc = 'objective-c', objcpp = 'objective-cpp', cuda = 'cuda-cpp' }
+        return t[ftype] or ftype
+    end,
+    capabilities = {
+        textDocument = {
+            completion = {
+                editsNearCursor = true,
+            },
+        },
+        offsetEncoding = { 'utf-8', 'utf-16' },
+    },
+}
