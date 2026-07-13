3975a29fb92d8d2ad9168164fdf20bdd34452456
Henron Ruan <t7x9q7@u.northwestern.edu>
2026-07-13 16:17:38 -0500

    added find all files (including hidden and gitignore) keybind for telescope

--- plugin/telescope.lua
+++ plugin/telescope.lua
@@ -18,6 +18,7 @@ require('telescope').setup({})
 local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>pa', function() builtin.find_files({ no_ignore = true, hidden = true }) end, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
