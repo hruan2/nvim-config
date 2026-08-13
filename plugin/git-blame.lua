ed212f41c4c582dc711867eca1797cfd6ef79575
Henron Ruan <t7x9q7@u.northwestern.edu>
2026-08-13 12:33:20 -0500

    updated git blame plugin file with notes and default off

--- plugin/git-blame.lua
+++ plugin/git-blame.lua
@@ -1,3 +1,7 @@
+-- if this plugin is not working, then double check that repository is "safe"
+-- i.e. run "git blame" on its own in shell to check
+-- if this is not the case, and the repository should be trusted, run the
+-- command "git config --global --add safe.directory <directory>"
 vim.pack.add({
     {
         src = "https://github.com/f-person/git-blame.nvim",
@@ -7,7 +11,7 @@ vim.pack.add({
 
 local git_blame = require("gitblame")
 git_blame.setup {
-    enabled = true,
+    enabled = false,
     message_template = " <summary> || <date> || <author> || <<sha>>",
     date_format = "%m-%d-%Y %H:%M:%S",
     display_virtual_text = 1,
