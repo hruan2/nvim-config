9f043c73ad974cc61db1c29883cb58ea7dc72a10
Henron Ruan <t7x9q7@u.northwestern.edu>
2026-09-03 00:50:10 -0500

    small updates to init lua file

--- init.lua
+++ init.lua
@@ -1,9 +1,12 @@
+vim.loader.enable()
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.g.have_nerd_font = true
 
 _G.Config = {
 	use_treesitter_parser = true,
 	use_nvim_treesitter = true,
 }
 
-require("remap")
 require("set")
+require("remap")
