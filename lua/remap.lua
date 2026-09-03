cc27c16ca4da4a0234225e94b186f0b9956fd8a4
Henron Ruan <t7x9q7@u.northwestern.edu>
2026-09-03 00:50:42 -0500

    added many new keymaps

--- lua/remap.lua
+++ lua/remap.lua
@@ -1,5 +1,3 @@
-vim.g.mapleader = " "
-
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -12,7 +10,44 @@ vim.api.nvim_set_keymap(
 	{ noremap = false, silent = false }
 )
 
-vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { noremap = true })
+vim.keymap.set(
+	"n",
+	"<leader>q",
+	vim.diagnostic.setloclist,
+	{ desc = "Open diagnostic [Q]uickfix list" }
+)
+
+vim.keymap.set(
+	"t",
+	"<Esc><Esc>",
+	"<C-\\><C-n>",
+	{ desc = "Exit terminal mode" }
+)
+
+vim.keymap.set(
+	"n",
+	"<C-h>",
+	"<C-w><C-h>",
+	{ desc = "Move focus to the left window" }
+)
+vim.keymap.set(
+	"n",
+	"<C-l>",
+	"<C-w><C-l>",
+	{ desc = "Move focus to the right window" }
+)
+vim.keymap.set(
+	"n",
+	"<C-j>",
+	"<C-w><C-j>",
+	{ desc = "Move focus to the lower window" }
+)
+vim.keymap.set(
+	"n",
+	"<C-k>",
+	"<C-w><C-k>",
+	{ desc = "Move focus to the upper window" }
+)
 
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
