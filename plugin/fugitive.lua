8b0061a336b17ab3af7493cf477dd147af54da63
Henron Ruan <t7x9q7@u.northwestern.edu>
2026-09-03 00:57:05 -0500

    added toggle for git fugitive shortcut

--- plugin/fugitive.lua
+++ plugin/fugitive.lua
@@ -5,4 +5,23 @@ vim.pack.add({
 	},
 })
 
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+local toggle_fugitive = function()
+	local winids = vim.api.nvim_list_wins()
+	for _, id in pairs(winids) do
+		local status = pcall(vim.api.nvim_win_get_var, id, "fugitive_status")
+
+		if status then
+			vim.api.nvim_win_close(id, false)
+			return
+		end
+	end
+
+	vim.cmd("Git")
+end
+
+vim.keymap.set(
+	"n",
+	"<leader>gs",
+	toggle_fugitive,
+	{ silent = true, desc = "toggle git status" }
+)
