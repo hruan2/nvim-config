d7f03e1810142157f189a496cdc9797e098cc4b3
Henron Ruan <t7x9q7@u.northwestern.edu>
2026-09-03 00:52:47 -0500

    updated telescope plugin

--- plugin/telescope.lua
+++ plugin/telescope.lua
@@ -2,31 +2,17 @@
 vim.api.nvim_create_autocmd("PackChanged", {
 	group = vim.api.nvim_create_augroup("pack-changed", { clear = false }),
 	callback = function(ev)
-		local plugin_name = ev.data.spec.name
-		local action = ev.data.kind
-
-		if not ev.data.active then
-			vim.cmd("packadd " .. plugin_name)
-		end
+		local name = ev.data.spec.name
+		local kind = ev.data.kind
+		local path = ev.data.path
 
 		if
-			plugin_name == "telescope-fzf-native.nvim"
-			and (action == "install" or action == "update")
+			name == "telescope-fzf-native.nvim"
+			and (kind == "install" or kind == "update")
+			and vim.fn.executable("make") == 1
 		then
-			vim.system({ "make", "clean" }, { cwd = ev.data.path }):wait()
-			local res = vim.system({ "make" }, { cwd = ev.data.path }):wait()
-
-			if vim.v.shell_error ~= 0 then
-				vim.notify(
-					"failed to compile telescope-fzf-native: " .. res,
-					vim.log.levels.ERROR
-				)
-			else
-				vim.notify(
-					"succesfully compiled telescope-fzf-native",
-					vim.log.levels.INFO
-				)
-			end
+			run_build(name, { "make", "clean" }, path)
+			run_build(name, { "make" }, path)
 		end
 	end,
 })
@@ -50,14 +36,22 @@ vim.pack.add({
 	{
 		src = "https://github.com/nvim-telescope/telescope-live-grep-args.nvim",
 	},
+	{
+		src = "https://github.com/nvim-telescope/telescope-ui-select.nvim",
+	},
+	{
+		src = "https://github.com/nvim-telescope/telescope-file-browser.nvim",
+	},
 })
 
 local telescope = require("telescope")
 telescope.setup({})
 require("nvim-web-devicons").setup()
 
-telescope.load_extension("live_grep_args")
 telescope.load_extension("fzf")
+telescope.load_extension("live_grep_args")
+telescope.load_extension("ui-select")
+telescope.load_extension("file_browser")
 
 local builtin = require("telescope.builtin")
 
@@ -66,6 +60,9 @@ vim.keymap.set("n", "<leader>pa", function()
 	builtin.find_files({ no_ignore = true, hidden = true })
 end, {})
 vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+vim.keymap.set("n", "<space>fb", function()
+	telescope.extensions.file_browser.file_browser()
+end, { desc = "telescope file browser" })
 
 vim.keymap.set(
 	"n",
@@ -77,18 +74,19 @@ vim.keymap.set("n", "<leader>ps", function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end, { desc = "telescope grep string with search" })
 
+-- disable these for now, as they don't work and need to do more research into
+-- figuring out what live grep is and whether I should use it or not
 vim.keymap.set(
 	"n",
 	"<leader>lg",
 	builtin.live_grep,
-	{ desc = "telescope live grep" }
+	{ desc = "telescope live grep across all workspace files" }
 )
-vim.keymap.set("n", "<leader>ls", function()
-	builtin.live_grep(
-		{ search = vim.fn.input("Live grep > ") },
-		{ desc = "telescope live grep with search " }
-	)
-end)
+vim.keymap.set("n", "<leader>/", function()
+	builtin.live_grep({
+		grep_open_files = true,
+	})
+end, { desc = "search for term in open files using live grep" })
 
 vim.keymap.set("n", "<leader><leader>", builtin.buffers, {})
 vim.keymap.set("n", "<leader>reg", builtin.registers, {})
