1f8597f6925fd5cb9c72504d3ee3ec8fcb4b1466
Henron Ruan <t7x9q7@u.northwestern.edu>
2026-09-06 16:46:51 -0500

    updated vimtex viewer to be skim for MacOS (Darwin) and zathura for Linux

--- plugin/vimtex.lua
+++ plugin/vimtex.lua
@@ -4,4 +4,11 @@ vim.pack.add({
 	},
 }, { load = true })
 
-vim.g.vimtex_view_method = "zathura"
+local os_name = vim.loop.os_uname().sysname
+
+-- skim for macos, zathura for linux
+if os_name == "Darwin" then
+	vim.g.vimtex_view_method = "skim"
+elseif os_name == "Linux" then
+	vim.g.vimtex_view_method = "zathura"
+end
